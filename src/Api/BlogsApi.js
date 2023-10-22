3d7942800de778d541e634951c4d7dc93958bb29
Ak <moorthimt15@gmail.com>
2023-10-22 13:39:09 +0530

    change api call function

--- src/Api/BlogsApi.js
+++ src/Api/BlogsApi.js
@@ -8,7 +8,7 @@ const API_URL =
 export const BlogData = async () => {
   try {
     const response = await axios.get(API_URL);
-    return response.data;
+    return response.data.articles;
   } catch (err) {
     console.log(err);
   }
